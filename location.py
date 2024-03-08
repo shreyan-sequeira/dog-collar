ff8753d35ab94cad28c87b89c62f04ff714cb9bf
shreyan-sequeira <160077439+shreyan-sequeira@users.noreply.github.com>
2024-03-08 15:55:27 +0000

    Update location.py
    added functionality for the logger client.

--- location.py
+++ location.py
@@ -11,28 +11,36 @@ messages = []
 '''Callback function that connects the broker
  Upon connection, subscribes to the topic. In this case, as owntracks publishes location data, 
  this what will be obtained from the messages.'''
+#08/03/2024: added a connection to the collar/logs topic
 def connection(client, userdata, flags, rc):
     if rc == 0:
         client.connected_flag = True
         print("Connected OK Returned code=", rc)
         client.subscribe("owntracks/SS/#")
+        client.subscribe("collar/logs")
     else:
         print("Connection Unsuccessful, Error message:", rc)
-
+#08/03/2024: data from the collar/logs topic is now saved to an array
 def on_message(client, userdata, msg):
     topic = msg.topic
     try:
         messages.append(msg.payload.decode("utf8"))
     except Exception as e:
         print(f"Cannot decode data on topic {topic}: {e}")
+    userdata['logs'].append(msg.payload.decode())
 
-client = mqtt.Client()
+def on_disconnect(client, userdata, rc):
+    print("Disconnected with result code "+str(rc))
+
+client = mqtt.Client(userdata={'logs': []})
 client.on_connect = connection
 client.on_message = on_message
+client.on_disconnect = on_disconnect
 
 client.username_pw_set("SS", "mqttBROKER")
 client.connect("broker.hivemq.com", 1883)
 
 #this ensures the code loops forever which is vital for recieving continuous realtime information
+client.loop_start()
 client.loop_forever()
 
